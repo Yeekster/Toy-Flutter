ab5248d58e1c8fd0f59146d1f216e84ed557317f
Kenton <kayee@mail.csuchico.edu>
2022-10-01 15:39:45 -0700

    Imported english_words library into the project and used to to create random word generation

--- new_darts/basic_functionality/lib/main.dart
+++ new_darts/basic_functionality/lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:english_words/english_words.dart';  //This imports the english words
 import 'package:flutter/material.dart';     //Lets the scaffold function be used that is called below
 
 void main() {
@@ -13,16 +14,19 @@ class MyApp extends StatelessWidget {       //Makes the app into a widget with t
 
   @override
   Widget build(BuildContext context) {      //build, a method that describes how to display the widget in terms of lower level widgets
+    final wordPairingVariable = WordPair.random();  //This line came from the package:english words library to make a random word
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(                       //A widget from the material library, provides a default app bar and holds widget tree
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(                 //body consists of the Center widget, the center widget has a child widget called Text
-          child: Text('Hello World'),       //Text prints based on Center as a subtree of Center to the application
+        //body: const Center(                 //body consists of the Center widget, the center widget has a child widget called Text
+        //  child: Text('Hello World'),       //Text prints based on Center as a subtree of Center to the application
+        body: Center(                                     //Replaced the  body above with this, note that it is no longer const
+          child: Text(wordPairingVariable.asPascalCase),  //Here we print the text using the variable declared above that is random
         ),
-      ),
-    );
+        ),
+      ); //Note that the Matrial App has a semi colon afterwards
   }
 }
